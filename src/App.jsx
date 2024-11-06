50d89609f8ffab9fd7c2536e6137ec626c419410
HazzaMore <mrhazar.moore@gmail.com>
2024-11-06 19:00:30 +0000

    Added Analytics page ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom"
+import Sidebar from "./components/Sidebar"
 import OverviewPage from "./pages/OverviewPage"
 import ProductsPage from "./pages/ProductsPage"
 import UsersPage from "./pages/UsersPage"
 import SalesPage from "./pages/SalesPage"
-import Sidebar from "./components/Sidebar"
 import OrdersPage from "./pages/OrdersPage"
+import AnalyticsPage from "./pages/AnalyticsPage"
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
           <Route path="/users" element={<UsersPage />} />
           <Route path="/sales" element={<SalesPage />} />
           <Route path="/orders" element={<OrdersPage />} />
+          <Route path="/analytics" element={<AnalyticsPage />} />
         </Routes>
 
       </div>
